65fb695922595a72cce5cd9a505e4883b0e48949
Hasan1284 <hasansevindik1284@gmail.com>
2020-05-01 12:24:45 +0200

    dongulerle ilgili uygulama

--- app.js
+++ app.js
@@ -114,11 +114,31 @@ let people = [
  * 
  */ 
 
+
+ var hak,can;
+ var tahmin,sayac = 0;
  var sayi = Math.floor((Math.random()*10)+1);
+can = Number(prompt('kaçkerede bileceksiniz?'));
+hak = can;
+console.log(sayi);
 
+while(hak>0){
+    hak--;
+    sayac++;
+    tahmin = Number(prompt('bir sayı giriniz'));
+    
+    if(sayi == tahmin){
+        console.log(`tebrikler ${sayac} defada bildiniz`);
+        console.log(`puan : ${100 -(100/can)*(sayac-1)}`);
+        break;
+    }else if(sayi > tahmin){
+        console.log('aşağı');
+    }else{
+        console.log('yukarı');
+    };
 
-dsdsadad
-jjhfj
-
-yeni branch
+    if(hak==0){
+        console.log('hakkınız bitti. sayı:'+sayi)
+    }
+}
 
